0feef8f15c3b67ef9e6b6873c472bad6504fcccf
Phillip Haberman <pjhab2020@gmail.com>
2022-07-11 15:24:43 +0800

    added a cleaning ipynb and py file and a model.py

--- cleaning.py
+++ cleaning.py
@@ -1,7 +1,26 @@
+# Import packages
 import pandas as pd
 
-path = 'data/btc.csv'
+# Function to load the csv as a dataframe
+def import_data():
+    path = 'data/btc.csv'
+    return pd.read_csv(path)
 
-df = pd.read_csv(path)
+# Function to rename the data columns
+def rename_columns(df):
+    df.columns = ['ds', 'y']
+    return df
 
-print(df.open_rate.mean())
+# Function to set datatype of columns
+def set_dtypes(df):
+    df['ds'] = pd.to_datetime(df['ds'])
+    return df
+
+# Function that combines previous functions into a pipline
+def pipeline():
+    df = import_data()
+    df = rename_columns(df)
+    df = set_dtypes(df)
+    return df.to_csv('data/btc_clean.csv', index=False)
+
+pipeline()
