a956aa35fa8c3c579a5e29005bac4ca89c63ddbc
Phillip Haberman <pjhab2020@gmail.com>
2022-07-11 16:48:56 +0800

    cleaning file and notebook

--- cleaning.py
+++ cleaning.py
@@ -1,7 +1,24 @@
+# Import packages
 import pandas as pd
 
-path = 'data/btc.csv'
+# Import data
+def import_data():
+    path = 'data/btc.csv'
+    return pd.read_csv(path)
 
-df = pd.read_csv(path)
+# Change column names
+def change_col_names(df):
+    df.columns = ['ds', 'y']
+    return df 
 
-print(df.open_rate.mean())
+# Export to csv
+def export_csv(df):
+    return df.to_csv('data/btc_clean.csv', index=False)
+
+# Pipeline
+def pipeline():
+    df = import_data()
+    df = change_col_names(df)
+    return export_csv(df)
+
+pipeline()
